0dea028f3548f6f6c1b8eba04943aee3f1c50fab
Andela Stevanovic <andjela.s166@gmail.com>
2021-12-03 21:28:15 -0500

    commit tree fd4e65ed9181896a8aad9cab468c225235a47d01

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file!
+    // const drivers = ['Sally', 'Bob', 'Freddy', 'Claudia'];
+    // const firstTwo = drivers.slice(0, 2);
+    // const lastTwo = drivers.slice(-2);
+    
+  function returnFirstTwoDrivers(driversArray) {
+      return driversArray.slice(0, 2);
+  }
+  function returnLastTwoDrivers(driversArray) {
+      return driversArray.slice(-2);
+  }
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(integer) {
+    return function() {
+       return integer * integer;
+    }
+}
+ 
+const fareDoubler = fare => fare * 2;
+const fareTripler = fare => fare * 3;
+let fareQuintupler = fare => fare * 4;
+
+function selectDifferentDrivers(driversArray, callBack) {
+  return callBack(driversArray);
+}
